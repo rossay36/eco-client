c01fd06933dc625d266107c2922f5e3dedb4e9bf
Obi Ross <obi75309@gmail.com>
2024-03-13 15:37:19 +0100

    made change on my token

--- src/ReqMethod.js
+++ src/ReqMethod.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
 
-// const token =
-//   "eyJpZCI6IjY1Y2RmMjk4MmYyOGYzMWY0ODdkNjE5ZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcxMDE0OTkzMiwiZXhwIjoxNzEwNDA5MTMyfQ.iuKPcsES4dYFTKbpUlTWMKgrW6TuHSNob5ldPEV8QMc";
-// const reqToken = TOKEN;
+const token =
+  "eyJpZCI6IjY1Y2RmMjk4MmYyOGYzMWY0ODdkNjE5ZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcxMDE0OTkzMiwiZXhwIjoxNzEwNDA5MTMyfQ.iuKPcsES4dYFTKbpUlTWMKgrW6TuHSNob5ldPEV8QMc";
+const reqToken = TOKEN;
 
 const TOKEN =
   JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user).currentUser
@@ -16,5 +16,5 @@ export const publicReq = axios.create({
 
 export const userReq = axios.create({
   baseURL: BASE_URL,
-  headers: { token: `Bearer ${TOKEN}` },
+  headers: { token: `Bearer ${token}` },
 });
